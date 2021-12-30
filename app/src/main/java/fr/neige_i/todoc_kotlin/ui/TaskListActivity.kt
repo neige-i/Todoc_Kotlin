5a586a8e994d791fb8f012ae24ce3b1dc277915f
neige-i <ibrahim.naji.rachdi@gmail.com>
2021-12-30 22:18:16 +0100

    Add resources: layouts, drawables, menu, colors, strings

--- app/src/main/java/fr/neige_i/todoc_kotlin/ui/TaskListActivity.kt
+++ app/src/main/java/fr/neige_i/todoc_kotlin/ui/TaskListActivity.kt
@@ -1,11 +1,12 @@
-package fr.neige_i.todoc_kotlin
+package fr.neige_i.todoc_kotlin.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.neige_i.todoc_kotlin.R
 
-class MainActivity : AppCompatActivity() {
+class TaskListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_task_list)
     }
 }
